9ec0798dcec67391cfaad011361256609971ab96
Administrator <Administrator@WV6MZ2GPQHH8MWW>
2018-10-30 17:09:48 +0800

    2

--- src/main/java/com/fs/utils/GeneratorDisplay.java
+++ src/main/java/com/fs/utils/GeneratorDisplay.java
@@ -4,10 +4,13 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.Resource;
+
 import org.mybatis.generator.api.MyBatisGenerator;
 import org.mybatis.generator.config.Configuration;
 import org.mybatis.generator.config.xml.ConfigurationParser;
 import org.mybatis.generator.internal.DefaultShellCallback;
+import org.springframework.util.ResourceUtils;
 
 public class GeneratorDisplay {
 
@@ -16,7 +19,7 @@ public class GeneratorDisplay {
 		List<String> warnings = new ArrayList<String>();
 		boolean overwrite = true;
 		//指定 逆向工程配置文件
-		File configFile = new File("generatorConfig.xml"); 
+		File configFile = ResourceUtils.getFile("classpath:config/generatorConfig.xml");
 		ConfigurationParser cp = new ConfigurationParser(warnings);
 		Configuration config = cp.parseConfiguration(configFile);
 		DefaultShellCallback callback = new DefaultShellCallback(overwrite);
